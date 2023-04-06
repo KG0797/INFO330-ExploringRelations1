0fecdfbaac714cece9985dd63f5a0974c76d05f8
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-06 09:13:41 -0700

    Update extra-1.sql

--- extra-1.sql
+++ extra-1.sql
@@ -1,4 +1,4 @@
  SELECT age
-   ...> FROM persons
-   ...> WHERE (gender = 'Female' AND title = 'Dr' AND city = 'West Palm Beach' AND state = 'Florida')
-   ...> OR (gender = 'Male' AND title = 'Rev' AND city = 'Phoenix' AND state = 'Arizona');
+ FROM persons
+ WHERE (gender = 'Female' AND title = 'Dr' AND city = 'West Palm Beach' AND state = 'Florida')
+ OR (gender = 'Male' AND title = 'Rev' AND city = 'Phoenix' AND state = 'Arizona');
