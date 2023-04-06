c1a8624af921cae2527d4eeece9483d91cb24330
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-06 09:15:39 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,3 +1,3 @@
 SELECT last_name || ',' || first_name AS full_name
-   ...> FROM persons
-   ...> WHERE age = (SELECT MIN(age) FROM persons);
+FROM persons
+WHERE age = (SELECT MIN(age) FROM persons);
