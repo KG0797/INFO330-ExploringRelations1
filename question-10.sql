a3e4c7a974006d661466264ed4efd1e18691e023
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-05 18:54:44 -0700

    Create question-10.sql

---
+++ question-10.sql
@@ -0,0 +1,3 @@
+ 
+      INSERT INTO persons(first_name, last_name, email, gender, title, age, address, city, state, postalcode, phone)
+ ...> VALUES('[Katherine]', '[Guo]', '[jguo25@uw.edu]', '[Female]', '[Ms]', 21, '[500 17th Ave]', '[Seattle]', '[Washington]', 98122, 929-345-3000);
