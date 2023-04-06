a6b8cea0ca788d5867c68cdb72205e0936b6a3de
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-05 22:24:21 -0700

    Create extra-1.sql

---
+++ extra-1.sql
@@ -0,0 +1,4 @@
+ SELECT age
+   ...> FROM persons
+   ...> WHERE (gender = 'Female' AND title = 'Dr' AND city = 'West Palm Beach' AND state = 'Florida')
+   ...> OR (gender = 'Male' AND title = 'Rev' AND city = 'Phoenix' AND state = 'Arizona');
